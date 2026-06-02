90703d0cd54b2e029af31598a1972590528091c2
Andre Jose de Brito <anjobrito@gmail.com>
2026-06-02 20:45:39 -0300

    chore: remove fluxo shortcut from layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,12 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <body>
-        {children}
-        <a className="process-shortcut" href="/fluxo">
-          Fluxo completo
-        </a>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
